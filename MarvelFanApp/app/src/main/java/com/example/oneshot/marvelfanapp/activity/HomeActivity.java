a154b96c5b2ca99f3a9c868dba0296e9294f6df4
DESKTOP-F6RU4LD\Oneshot <gonzalezac1992@gmail.com>
2018-07-25 13:59:45 +0200

    Add basic toolbar

--- MarvelFanApp/app/src/main/java/com/example/oneshot/marvelfanapp/activity/HomeActivity.java
+++ MarvelFanApp/app/src/main/java/com/example/oneshot/marvelfanapp/activity/HomeActivity.java
@@ -3,19 +3,24 @@ package com.example.oneshot.marvelfanapp.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
+
+
 
 import com.example.oneshot.marvelfanapp.R;
 
 
 public class HomeActivity extends AppCompatActivity {
-
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        toolbar =(Toolbar) findViewById(R.id.toolbarHome);
+        setSupportActionBar(toolbar);
 
       clickControl();
     }
